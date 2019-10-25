922f42a0a1185a512e2fcfef43e64ba40cf80616
HIDENORI TANAKA <hidenori091985@yahoo.co.jp>
2019-10-25 09:17:42 +0900

    add titleHeader

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="todoVue">
+    <div id="headerId">
+      <h1>TODO List</h1>
+    </div>
+    <div id="inputId"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
